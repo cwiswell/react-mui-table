85c8ce0b75949aa7ed445a4b67c8fe76bcf882ea
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-23 18:48:32 -0400

    Update test.ts to use muitable

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
-import ExampleComponent from './'
+import MuiTable from './index'
 
-describe('ExampleComponent', () => {
+describe('MuiTable', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(MuiTable).toBeTruthy();
   })
 })
