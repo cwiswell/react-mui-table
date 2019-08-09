bd8a16a72cdfe30dc36173d03b4853ae31cde6ad
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-09 09:56:36 -0400

    Change table header to be functional component

--- src/table-header.tsx
+++ src/table-header.tsx
@@ -8,29 +8,27 @@ import HeaderClass from './headerClass';
 
 //import styles from './styles.css'
 
-export type Props = { options: Array<HeaderClass> }
+type Props = { options: Array<HeaderClass> }
 
-export default class MuiTableHeader extends Component<Props> {
-
-  _renderCells: any = () => {
-    return this.props.options.map((item, key) => {
-        return (<TableCell key={key}>
-          {item.displayName}
-        </TableCell>)
-      }
-    );
+const MuiTableHeader: React.FC<Props> = (props) => {
+  const _renderCells: any = () => {
+    return props.options.map((item, key) => {
+      return (<TableCell key={key}>
+        {item.displayName}
+      </TableCell>)
+    });
   }
 
-  render() {
-    let cells = this._renderCells();
-    return (
-      <Fragment>
-        <TableHead>
-          <TableRow>
-            {cells}
-          </TableRow>
-        </TableHead>
-      </Fragment>
-    )
-  }
+  let cells = _renderCells();
+  return (
+    <Fragment>
+      <TableHead>
+        <TableRow>
+          {cells}
+        </TableRow>
+      </TableHead>
+    </Fragment>
+  );
+
 }
+export default MuiTableHeader
