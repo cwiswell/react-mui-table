cf6c5d9a7110123390b6b4e7d2435b16551c1b34
Craig A Wiswell <cwiswell63@gmail.com>
2019-08-09 09:57:05 -0400

    Remove unneeded import

--- src/table-header.tsx
+++ src/table-header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Fragment, Component } from 'react';
+import { Fragment } from 'react';
 
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
