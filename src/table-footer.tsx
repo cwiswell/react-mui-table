676e48584a0f0c95a079cbd99a75853e79c1074d
Craig A Wiswell <cwiswell63@gmail.com>
2019-11-09 02:14:22 -0500

    Update table footer to use functional component

--- src/table-footer.tsx
+++ src/table-footer.tsx
@@ -1,48 +1,40 @@
 import * as React from 'react';
-import { Fragment, Component} from 'react';
+import { Fragment } from 'react';
 
 import TableRow from '@material-ui/core/TableRow';
 import TableFooter from '@material-ui/core/TableFooter';
 import TablePagination from '@material-ui/core/TablePagination';
 
-export type Props = { 
-    count: number ,
-    rowsPerPage: number,
-    currentPage: number,
-    //Type the below functions appropriately
-    handleChangePage: any,
-    handleChangeRowsPerPage: any
+type MuiTableFooterProps = {
+  count: number,
+  rowsPerPage: number,
+  currentPage: number,
+  //Type the below functions appropriately
+  handleChangePage: any,
+  handleChangeRowsPerPage: any
 }
 
-export default class MuiTableFooter extends Component<Props> {
-  render() {
-    const {
-      count,
-      rowsPerPage,
-      currentPage,
-      handleChangePage,
-      handleChangeRowsPerPage
-    } = this.props
-
-    return (
-      <Fragment>        
-        <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                colSpan={3}
-                count={count}
-                rowsPerPage={rowsPerPage}
-                page={currentPage}
-                SelectProps={{
-                  native: true,
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-              />
-            </TableRow>
-          </TableFooter>
-      </Fragment>
-    )
-  }
+const MuiTableFooter: React.FC<MuiTableFooterProps> = (props) => {
+  return (
+    <Fragment>
+      <TableFooter>
+        <TableRow>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            colSpan={3}
+            count={props.count}
+            rowsPerPage={props.rowsPerPage}
+            page={props.currentPage}
+            SelectProps={{
+              native: true,
+            }}
+            onChangePage={props.handleChangePage}
+            onChangeRowsPerPage={props.handleChangeRowsPerPage}
+          />
+        </TableRow>
+      </TableFooter>
+    </Fragment>
+  );
 }
+
+export default MuiTableFooter;
